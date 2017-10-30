8a181dac90560325947a8289489edfbade453747
ohadschn <ohad188@gmail.com>
2017-10-30 03:32:17 +0200

    images.js - minor improvements

--- assets/js/images.js
+++ assets/js/images.js
@@ -1,15 +1,16 @@
 var cl = cloudinary.Cloudinary.new({cloud_name: "ohad188"});
-
-$heroDiv = $('<div class="page__hero"></div>')
-$heroDiv.insertAfter(".masthead");
-$heroDiv.append(cl.image("sergei-akulich-39911-cropped_navwyr", 
+var img = cl.image("sergei-akulich-39911-cropped_navwyr", 
 { 
     width: "auto", 
     dpr: "auto", 
     responsive: "true",
     crop: "scale", 
     responsive_placeholder: "blank"
-}));
-$heroDiv.find("img").addClass("page__hero-image");
+});
+$(img).addClass("page__hero-image");
+
+$heroDiv = $('<div class="page__hero"></div>')
+$heroDiv.append(img);
+$heroDiv.insertAfter(".masthead");
 
 cl.responsive();
