14d0548cae25c11498af4a5c8ad7952eeb38845a
ohadschn <ohad188@gmail.com>
2017-10-30 03:23:45 +0200

    Add page__hero-image class to Cloudinary hero image

--- assets/js/images.js
+++ assets/js/images.js
@@ -1,12 +1,15 @@
 var cl = cloudinary.Cloudinary.new({cloud_name: "ohad188"});
-$('<div class="page__hero"></div>').insertAfter(".masthead");
-$(".page__hero").append(cl.image("sergei-akulich-39911-cropped_navwyr", 
+
+$heroDiv = $('<div class="page__hero"></div>')
+$heroDiv.insertAfter(".masthead");
+$heroDiv.append(cl.image("sergei-akulich-39911-cropped_navwyr", 
 { 
     width: "auto", 
     dpr: "auto", 
     responsive: "true",
     crop: "scale", 
-    responsive_placeholder: "blank",
-    class: "cld-responsive page__hero-image"
+    responsive_placeholder: "blank"
 }));
+$heroDiv.find("img").addClass("page__hero-image");
+
 cl.responsive();
