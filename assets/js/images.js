6bd1629f3448fbc400640cffc09585ffbfe28e89
ohadschn <ohad188@gmail.com>
2017-11-14 00:08:39 +0200

    Cloudinary - run after document is ready

--- assets/js/images.js
+++ assets/js/images.js
@@ -1,16 +1,18 @@
-var cl = cloudinary.Cloudinary.new({cloud_name: "ohad188"});
-var img = cl.image("sergei-akulich-39911-cropped_navwyr", 
-{ 
-    width: "auto", 
-    dpr: "auto", 
-    responsive: "true",
-    crop: "scale", 
-    responsive_placeholder: "blank",
-    className: "page__hero-image"
+$( document ).ready(function() {
+    var cl = cloudinary.Cloudinary.new({cloud_name: "ohad188"});
+    var img = cl.image("sergei-akulich-39911-cropped_navwyr", 
+    { 
+        width: "auto", 
+        dpr: "auto", 
+        responsive: "true",
+        crop: "scale", 
+        responsive_placeholder: "blank",
+        className: "page__hero-image"
+    });
+    
+    $heroDiv = $('<div class="page__hero"></div>');
+    $heroDiv.append(img);
+    $heroDiv.insertAfter(".masthead");
+    
+    cl.responsive();
 });
-
-$heroDiv = $('<div class="page__hero"></div>');
-$heroDiv.append(img);
-$heroDiv.insertAfter(".masthead");
-
-cl.responsive();
